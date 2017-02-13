e49e55d8178d1f07e5cc65e76a1abfdd20f556f3
Clive <starlwj@gmail.com>
2017-02-14 06:44:18 +0800

    for debug error, remove html/css/js in welcome page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,20 +17,3 @@
 //= require bootstrap/dropdown
 //= require bootstrap/carousel
 //= require_tree .
-
-$(document).ready(function() {
-
-  $('.slick-slider').bxSlider({
-    minSlides: 1,
-    maxSlides: 1,
-    controls: false
-  });
-
-  $( ".explore-button" ).hover(function() {
-    $( this ).parent().addClass("hovered-card");
-  }, function() {
-    $( this ).parent().removeClass("hovered-card");
-  }
-);
-
-});
